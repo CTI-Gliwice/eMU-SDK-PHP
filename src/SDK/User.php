f8b225c7e87b30da9f5962557288107bd76731b2
Paweł Przespolewski <72556823+AbyssMorganCTI@users.noreply.github.com>
2024-01-23 11:12:05 +0100

    UserNote

--- src/SDK/User.php
+++ src/SDK/User.php
@@ -14,7 +14,7 @@ class User extends Core {
 	}
 
 	public function list(?string $search = null, bool $with_trashed = false) : array|false {
-		$this->set_response($this->request->post("$this->api_url/list", ['search' => $search, 'trashed' => $with_trashed]));
+		$this->set_response($this->request->post("$this->api_url/list", ['search' => $search, 'with_trashed' => $with_trashed]));
 		if($this->get_response_code() != 200) return false;
 		return $this->get_response_data();
 	}
