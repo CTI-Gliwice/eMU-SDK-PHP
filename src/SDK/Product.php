ce8220e129e5de62619139057d3cf0d1fbdfb2e2
Paweł Przespolewski <72556823+AbyssMorganCTI@users.noreply.github.com>
2024-02-21 11:42:19 +0100

    SDK Gratis i Produkt

---
+++ src/SDK/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace eMU\SDK;
+
+use eMU\Traits\ElementEditor;
+use eMU\Traits\AttachmentsEditor;
+use eMU\Traits\TableQuery;
+
+class Product extends Core {
+
+	use ElementEditor;
+	use AttachmentsEditor;
+	use TableQuery;
+	
+	protected string $api_url;
+
+	public function __construct(string $app_url, ?array $auth = null){
+		parent::__construct($app_url);
+		if(!is_null($auth)) $this->request->setHeader($auth);
+		$this->api_url = "$this->app_url/emu/product";
+	}
+
+}
+
+?>
