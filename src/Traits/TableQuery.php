c03d696c738c86124e7697fbb72be370ad3d7db7
Paweł Przespolewski <pawelprzespolewski@cti.org.pl>
2024-12-06 10:18:41 +0100

    Update TableQuery.php

--- src/Traits/TableQuery.php
+++ src/Traits/TableQuery.php
@@ -6,8 +6,8 @@ namespace eMU\Traits;
 
 trait TableQuery {
 
-	public function query(array $filter = [], int $offset = 0, int $limit = 100, array $extra_data = []) : array|false {
-		$this->set_response($this->request->post("$this->api_url/query", ['filter' => $filter, 'offset' => $offset, 'limit' => $limit, 'extra_data' => $extra_data]));
+	public function query(array $filter = [], int $offset = 0, int $limit = 100, array $extra_data = [], array $special_fields = []) : array|false {
+		$this->set_response($this->request->post("$this->api_url/query", ['filter' => $filter, 'offset' => $offset, 'limit' => $limit, 'extra_data' => $extra_data, 'special_fields' => $special_fields]));
 		if($this->get_response_code() != 200) return false;
 		return $this->get_response_data();
 	}
