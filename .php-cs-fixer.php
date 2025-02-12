fe668dbbf07ed0cb4740563c6b6ba244a3300291
Paweł Przespolewski <pawelprzespolewski@cti.org.pl>
2025-02-12 13:42:55 +0100

    Create .php-cs-fixer.php

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()->in(__DIR__)->name('*.php')->exclude('vendor')->exclude('resources')->exclude('storage')->notPath('cache');
+
+$config = [
+	'array_indentation' => true,
+	'binary_operator_spaces' => ['default' => 'single_space'],
+	'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+	'no_trailing_whitespace' => true,
+	'whitespace_after_comma_in_array' => true,
+];
+
+return (new Config())->setRules($config)->setIndent("\t")->setLineEnding("\n")->setFinder($finder);
